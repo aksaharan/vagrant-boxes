85f78f9c8378a02c719e4d5414bc39458e07fe2d
Anil Kumar <aksaharan@yahoo.com>
2014-06-23 14:19:12 -0400

    parameterized the rpm install

--- MongoDB/mms-automation-base/config/install.sh
+++ MongoDB/mms-automation-base/config/install.sh
@@ -2,11 +2,25 @@
 
 set -ex
 
+# Assumes the RPM file to be available in the /vagrant mounter shared directory. If not, uses the online URL
+function installRPM {
+	RPMFILE="/vagrant/$1"
+	if [ -f "$RPMFILE" -a -r "$RPMFILE" ]; then
+		sudo yum --assumeyes --cacheonly install "$RPMFILE"
+	elif [ -n "$2" ]; then
+		echo "Local RPM file not found, will try to install from internet location"
+		sudo yum --assumeyes --cacheonly install "$2"
+	else
+		echo "Failed to install the RPM for following options [RPMFile: $RPMFILE, URL: $2]"
+	fi
+}
+
+
 echo "Installing MongoDB Shell Utilities"
-sudo yum -y install http://downloads-distro.mongodb.org/repo/redhat/os/x86_64/RPMS/mongodb-org-shell-2.6.2-1.x86_64.rpm
+installRPM "mongodb-org-shell-2.6.2-1.x86_64.rpm" "http://downloads-distro.mongodb.org/repo/redhat/os/x86_64/RPMS/mongodb-org-shell-2.6.2-1.x86_64.rpm"
 
 echo "Installing MMS Automation Agent"
-sudo yum -y install https://mms.mongodb.com/download/agent/automation/mongodb-mms-automation-agent-manager-latest.x86_64.rpm
+installRPM "mongodb-mms-automation-agent-manager-latest.x86_64.rpm" "https://mms.mongodb.com/download/agent/automation/mongodb-mms-automation-agent-manager-latest.x86_64.rpm"
 
 echo "Updating the MMS Automation Agent configuration file"
 sudo cp /etc/mongodb-mms/automation-agent.config /etc/mongodb-mms/automation-agent.config.orig
@@ -23,3 +37,10 @@ sudo chown mongod:mongod /data/db
 
 ls -ld /data/db
 ls -ltr /data/db
+
+echo "following iptables filters created by default on Linux are being cleared now"
+sudo iptables -L
+sudo iptables -F
+
+echo "following iptables filters after clearing it up"
+sudo iptables -L
